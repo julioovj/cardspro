774e22b0eb623b4f99c7b48af64ecb8b99b786f1
Jonn <68270215+julioovj@users.noreply.github.com>
2024-05-22 15:24:42 -0300

    Update types.ts

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -1,51 +1,3 @@
-export type ClassicOption = {
-    thumbnailImage?: string;
-    backgroundColor?: string;
-    backgroundImage?: string;
-    imageDarkness?: number;
-    progress?: number;
-    progressColor?: string;
-    progressBarColor?: string;
-    name?: string;
-    nameColor?: string;
-    author?: string;
-    authorColor?: string;
-    startTime?: string;
-    endTime?: string;
-    timeColor?: string
-}
-
-export type ClassicProOption = {
-    thumbnailImage?: string;
-    backgroundColor?: string;
-    backgroundImage?: string;
-    imageDarkness?: number;
-    progress?: number;
-    progressColor?: string;
-    progressBarColor?: string;
-    name?: string;
-    nameColor?: string;
-    author?: string;
-    authorColor?: string;
-    startTime?: string;
-    endTime?: string;
-    timeColor?: string;
-}
-
-export type DynamicOption = {
-    thumbnailImage?: string;
-    backgroundColor?: string;
-    backgroundImage?: string;
-    imageDarkness?: number;
-    progress?: number;
-    progressColor?: string;
-    progressBarColor?: string;
-    name?: string;
-    nameColor?: string;
-    author?: string;
-    authorColor?: string;
-}
-
 export type DynamicProOption = {
     thumbnailImage?: string;
     backgroundColor?: string;
@@ -62,16 +14,4 @@ export type DynamicProOption = {
     requested?: string;
     userImage?: string;
     plataform?: string;
-}
-
-export type MiniOption = {
-    thumbnailImage: string;
-    backgroundColor: string;
-    backgroundImage?: string;
-    imageDarkness?: number;
-    menuColor: string;
-    progress: number;
-    progressColor: string;
-    progressBarColor: string;
-    paused: boolean;
 }
