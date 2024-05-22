7f57f9af95dd4515b1b0a26b9fad7ec9c7d4990d
Jonn <68270215+julioovj@users.noreply.github.com>
2024-05-22 15:24:55 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,2 @@
 export * from "./typings/types"
-export * from "./themes/classic"
-export * from "./themes/classicpro"
-export * from "./themes/dynamic"
-export * from "./themes/mini"
 export * from "./themes/dynamicpro"
