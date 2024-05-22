c38f75a2331591c5f7afedfe3f5ef899d86effbc
Jonn <68270215+julioovj@users.noreply.github.com>
2024-05-22 13:49:23 -0300

    Update types.ts

--- src/typings/types.ts
+++ src/typings/types.ts
@@ -46,6 +46,24 @@ export type DynamicOption = {
     authorColor?: string;
 }
 
+export type DynamicProOption = {
+    thumbnailImage?: string;
+    backgroundColor?: string;
+    backgroundImage?: string;
+    imageDarkness?: number;
+    progress?: number;
+    progressColor?: string;
+    progressBarColor?: string;
+    name?: string;
+    nameColor?: string;
+    author?: string;
+    authorColor?: string;
+    duration?: string;
+    requested?: string;
+    userImage?: string;
+    plataform?: string;
+}
+
 export type MiniOption = {
     thumbnailImage: string;
     backgroundColor: string;
